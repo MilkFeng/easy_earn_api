cbaab69658a0b64947fe9371ffb3d373568bd5b0
milkfeng <3107055729@qq.com>
2023-11-02 13:52:34 +0800

    update

--- src/task/task.js
+++ src/task/task.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { verify_body } = require('../../common/utils.js');
-const { passport, db } = require('../../user/passport.js');
+const { verify_body } = require('../common/utils.js');
+const { passport, db } = require('../user/passport.js');
 
 const router = express.Router();
 
