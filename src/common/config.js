fc13d705af82939f7a132b926ebe9f61b2ed2916
milkfeng <3107055729@qq.com>
2023-11-03 01:40:19 +0800

    debug

--- src/common/config.js
+++ src/common/config.js
@@ -3,19 +3,26 @@ const path = require('path');
 
 let _config = {};
 const config_path = path.join(__dirname, '../../config.json');
-if (!fs.existsSync(config_path)) {
-    _config = null;
-} else _config = JSON.parse(fs.readFileSync(config_path, 'utf8'));
+if (fs.existsSync(config_path)) _config = JSON.parse(fs.readFileSync(config_path, 'utf8'));
 
 const config = {
-    contract_id: _config.contract_id || 'rho:id:98x5p8qypec8u7pagiejkuecgzi9ehm5ewui7xwmzk11ttypequgpx',
+  contract_id: _config.contract_id || 'rho:id:98x5p8qypec8u7pagiejkuecgzi9ehm5ewui7xwmzk11ttypequgpx',
 };
+
+if (!fs.existsSync(config_path)) {
+  try {
+    fs.writeFile(config_path, JSON.stringify(config, null, 2), _ => { });
+    console.log('config.json was created');
+  } catch (err) {
+    console.error('cannot create config.json. error: ', err);
+  }
+}
 
 const keys = Object.keys(config);
 keys.forEach((key) => {
-    console.log('load ' + key + ': ' + config[key]);
+  console.log('load ' + key + ': ' + config[key]);
 });
 
 module.exports = {
-    config,
+  config,
 }
